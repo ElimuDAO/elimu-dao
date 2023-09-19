d9d270ec6e85643151d50eb121c916754db5ba20
@bashybaranaba <bashybaranaba@gmail.com>
2023-09-20 01:03:12 +0300

    contract update

--- contracts/Elimu.sol
+++ contracts/Elimu.sol
@@ -9,18 +9,35 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Elimu is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
+    Counters.Counter private _courseIds;
     Counters.Counter private _tokenIds;
+
+    struct Course {
+        address creator;
+        string courseData;
+        address[] members;
+    }
 
     struct Certificate{
         uint256 courseId;
         string details;
     }
     
+    Course[] public courses;
     mapping(uint256 => Certificate) public certificates;
+
+    event CourseCreated(uint256 indexed id, address indexed creator, string projectData);
 
     event CertificateMinted(uint256 tokenId, address owner, uint256 courseId, string details, string tokenUri);
 
     constructor() ERC721("Elimu Cert", "ELM") {}
+
+     function createCourse(string memory _courseData) public {
+        _courseIds.increment();
+        uint256 newProjectId = _courseIds.current(); 
+        courses.push(Course(msg.sender,_courseData,new address[](0)));
+        emit CourseCreated(newProjectId, msg.sender, _courseData);
+    }
 
     function mintCertificate(address recipient, uint256 courseId, string memory details,  string memory tokenUri) public returns (uint256) {
         _tokenIds.increment();
